3b93b4fa81db758a799cf694539fe9b3ccf9bf9e
Dongkuk <dongkuk5411@nhnnext.org>
2014-01-17 11:31:38 +0900

    Server Initializer로 추상화 함

--- src/ServerInitializer.java
+++ src/ServerInitializer.java
@@ -3,6 +3,7 @@ import logger.ConsoleLogger;
 import logger.Logger;
 import protocol.ProtocolFactory;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 
 /**
@@ -12,14 +13,15 @@ import java.net.ServerSocket;
  * @details 소켓, 로거, 프로토콜 팩토리를 디스패처에 전달한다.
  * @date 2014-01-16
  */
-public class ThreadMain {
 
-    /**
-     * @param args 포트번호, 프로토콜명, 디스패처명이 있는 콘솔 입력 함수
-     * @throws Exception
-     * @brief 메인 메서드
-     * @details 콘솔에서 포트번호, 프로토콜명, 디스패처명을 입력받아 적절한 소켓, 프로토콜팩토리, 디스패처를 생성한다.
-     */
+/**
+ * @param args 포트번호, 프로토콜명, 디스패처명이 있는 콘솔 입력 함수
+ * @throws Exception
+ * @brief 메인 메서드
+ * @details 콘솔에서 포트번호, 프로토콜명, 디스패처명을 입력받아 적절한 소켓, 프로토콜팩토리, 디스패처를 생성한다.
+ */
+public class ServerInitializer {
+
     public static void main(String[] args) throws Exception {
 
         if (args.length != 3)
@@ -29,6 +31,13 @@ public class ThreadMain {
         int servPort = Integer.parseInt(args[0]);
         String protocolName = args[1];
         String dispatcherName = args[2];
+
+        ServerInitializer serverInitializer = new ServerInitializer();
+        serverInitializer.initializeServer(servPort, protocolName, dispatcherName);
+    }
+
+    public void initializeServer(int servPort, String protocolName, String dispatcherName)
+            throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 
         ServerSocket servSock = new ServerSocket(servPort);
         Logger logger = new ConsoleLogger();
